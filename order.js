6308fcddcdb6ad8ef40f5c9fdab054b4779fc425
Cootessa <piecesofthepeople911@hotmail.com>
2023-03-10 18:30:54 -0500

    Still figuring out how to do all this

--- order.js
+++ order.js
@@ -10,17 +10,4 @@ let custAddress;
 let numItem;
 let myItem;
 
-document.getElementById("orderButton").onclick = function(){
-  myItem = document.getElementById("myItem").value;
-  console.log(myItem);
-  numItem = document.getElementById("numItem").value;
-  console.log(numItem);
-  custName = document.getElementById("custName").value;
-  console.log(custName);
-  custAddress = document.getElementById("custAddress").value;
-  console.log(custAddress);
-  custPhone = document.getElementById("custPhone").value;
-  console.log(custPhone);
-  document.getElementById("testing").innerHTML = "You want " + myItem + numItem + ". Your name is " + custName + 
-  " and you live at " + custAddress + ". And your number is " + custPhone;
-}
+document.getElementById("orderButton").onclick = function(){}
