2390ffbc86b1aa55575e1b75b37e3fa6716d0642
natlyantias <98179490+natlyantias@users.noreply.github.com>
2023-03-12 20:43:03 -0400

    Update order.js

--- order.js
+++ order.js
@@ -1,6 +1,26 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'natly',
+  password: 'jUN24ior20$',
+  database: 'bakery'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM orders', (error, results, fields) => {
+  if (error) {
+    console.error('Failed to retrieve orders:', error);
+    return;
+  }
+  console.log('Orders:', results);
+});
+
 function redirectToOrder() {
   window.location.href = "orderindex.html";
 }
 
 document.getElementById("order-button").addEventListener("click", redirectToOrder);
 
+connection.end();
