f79418edeaaac94c0c0c5779af60db664a125a88
natlyantias <98179490+natlyantias@users.noreply.github.com>
2023-03-12 21:17:36 -0400

    Update order.js

--- order.js
+++ order.js
@@ -4,23 +4,3 @@ function redirectToOrder() {
 
 document.getElementById("order-button").addEventListener("click", redirectToOrder);
 
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'natly',
-  password: 'jUN24ior20$',
-  database: 'bakery'
-});
-
-connection.connect();
-
-connection.query('SELECT * FROM orders', (error, results, fields) => {
-  if (error) {
-    console.error('Failed to retrieve orders:', error);
-    return;
-  }
-  console.log('Orders:', results);
-});
-
-connection.end();
