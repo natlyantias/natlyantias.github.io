7e41065e6c3f812c0fac2b83796ef0f20f09e525
natlyantias <98179490+natlyantias@users.noreply.github.com>
2023-03-14 13:57:51 -0400

    Create confirmation.js

---
+++ confirmation.js
@@ -0,0 +1,5 @@
+function redirectToConfirmation() {
+  window.location.href = "confirmationindex.html";
+}
+
+document.getElementById("confirmation-button").addEventListener("click", redirectToConfirmation);
