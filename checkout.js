eba42aab29a36064555c8b650aac009be41d5f87
natlyantias <98179490+natlyantias@users.noreply.github.com>
2023-03-14 11:32:34 -0400

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -4,5 +4,5 @@ const form = document.getElementById('order-form');
 const submitButton = document.getElementById('submit-button');
 
 submitButton.addEventListener('click', function() {
-  window.location.href = 'https://natlyantias.github.io/checkout.html';
+  window.location.href = 'https://natlyantias.github.io/checkoutindex.html';
 });
