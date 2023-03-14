0807728e5a0ddc6de307aaa1b052664df5ee8e9d
natlyantias <98179490+natlyantias@users.noreply.github.com>
2023-03-14 18:04:46 -0400

    Update order.js

--- order.js
+++ order.js
@@ -2,4 +2,4 @@ function redirectToOrder() {
   window.location.href = "orderindex.html";
 }
 
-document.getElementById("submit-button").addEventListener("click", redirectToOrder);
+document.getElementById("order-button").addEventListener("click", redirectToOrder);
