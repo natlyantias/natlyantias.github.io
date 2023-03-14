974d73fb5ec6486877e4c2e1ceb4ef88b6fe6e73
natlyantias <98179490+natlyantias@users.noreply.github.com>
2023-03-14 13:35:15 -0400

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,5 +1,38 @@
-function redirectToContact() {
-  window.location.href = "checkoutindex.html";
+
+
+// read selected items from localStorage
+var items = JSON.parse(localStorage.getItem('items'));
+
+// update order summary table
+var table = document.querySelector('table');
+var total = 0;
+for (var i = 0; i < items.length; i++) {
+var row = table.insertRow(-1);
+var itemCell = row.insertCell(0);
+var quantityCell = row.insertCell(1);
+var priceCell = row.insertCell(2);
+itemCell.innerHTML = items[i];
+
+// calculate price based on selected item
+var price = 0;
+switch(items[i]) {
+case 'Chocolate Cake':
+price = 10;
+break;
+case 'Croissants':
+price = 2;
+break;
+case 'Blueberry Muffins':
+price = 2;
+break;
+default:
+break;
+}
+quantityCell.innerHTML = '1';
+priceCell.innerHTML = '$' + price.toFixed(2);
+total += price;
 }
 
-document.getElementById("submit-button").addEventListener("click", redirectToContact);
+// update total price
+var totalPrice = document.querySelector('h2');
+totalPrice.innerHTML = 'Total: $' + total.toFixed(2);
