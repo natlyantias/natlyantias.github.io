ac0f97e94345749cfedebd7fde8d2313ba3836c4
natlyantias <98179490+natlyantias@users.noreply.github.com>
2023-03-14 13:33:30 -0400

    Update order.js

--- order.js
+++ order.js
@@ -1,6 +1,18 @@
-function redirectToOrder() {
-  window.location.href = "orderindex.html";
-}
 
-document.getElementById("order-button").addEventListener("click", redirectToOrder);
+
+document.getElementById('order-button').addEventListener('click', function(event) {
+  event.preventDefault();
+  // get selected items
+  var items = [];
+  var checkboxes = document.getElementsByName('croissant');
+  for (var i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      items.push(checkboxes[i].value);
+    }
+  }
+  // store selected items in localStorage
+  localStorage.setItem('items', JSON.stringify(items));
+  // redirect to checkout page
+  window.location.href = 'checkoutindex.html';
+});
 
