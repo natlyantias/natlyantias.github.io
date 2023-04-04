4364db8b7d066006ca022d4786330ea1de4ec29f
Cootessa <piecesofthepeople911@hotmail.com>
2023-04-03 22:43:36 -0400

    I can't remember what I changed

--- review.js
+++ review.js
@@ -1,15 +1,9 @@
 //Possibly remove
 
-document.addEventListener('DOMContentLoaded', function(){
-  fetch('https://natlyantias.github.io/getAll')
-  .then(response => response.json())
-  .then(data => console.log(data));
-});
 
 function redirectToReview() {
   window.location.href = "reviewindex.html";
 }
 
 document.getElementById("review-button").addEventListener("click", redirectToReview);
-
 
