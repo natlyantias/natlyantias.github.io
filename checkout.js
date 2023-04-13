c3b75c6b5d5eb5200ec03fcf1432efd1e51ce8ca
natlyantias <98179490+natlyantias@users.noreply.github.com>
2023-04-13 11:19:32 -0400

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,4 +1,36 @@
- function redirectToCheckout() {
+const menuItems = document.querySelectorAll('.menu-item');
+menuItems.forEach(item => {
+  const addToCartButton = item.querySelector('.add-to-cart');
+  addToCartButton.addEventListener('click', () => {
+    const name = item.dataset.name;
+    const price = item.dataset.price;
+    addToCart(name, price);
+  });
+});
+
+const cart = [];
+
+function addToCart(name, price) {
+  const item = { name, price };
+  cart.push(item);
+  updateOrderSummary();
+}
+
+const orderSummaryElement = document.querySelector('#order-summary');
+const totalElement = document.querySelector('#total');
+
+function updateOrderSummary() {
+  let total = 0;
+  let html = '';
+  cart.forEach(item => {
+    html += `<li>${item.name} - ${item.price}</li>`;
+    total += parseFloat(item.price);
+  });
+  orderSummaryElement.innerHTML = html;
+  totalElement.textContent = `Total: $${total.toFixed(2)}`;
+}
+
+function redirectToCheckout() {
   window.location.href = "checkoutindex.html";
 }
 
