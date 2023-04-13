e4406af1972b8ff1fde238aeb6aa251655acd54d
natlyantias <98179490+natlyantias@users.noreply.github.com>
2023-04-13 00:00:35 -0400

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,5 +1,27 @@
 function redirectToCheckout() {
-  window.location.href = "checkoutindex.html";
+  fetch('/create-checkout-session', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      items: [{ id: 'butterCrois', quantity: 1 }]
+    })
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(session) {
+    return stripe.redirectToCheckout({ sessionId: session.id });
+  })
+  .then(function(result) {
+    if (result.error) {
+      alert(result.error.message);
+    }
+  })
+  .catch(function(error) {
+    console.error('Error:', error);
+  });
 }
 
 document.getElementById("checkout-button").addEventListener("click", redirectToCheckout);
