eb2a9d67bebb675f9dedf8816669c38bd587c2a8
natlyantias <98179490+natlyantias@users.noreply.github.com>
2023-04-12 21:28:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,61 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
-var con = require('./db');
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended:true}));
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'natly',
+  password: 'jUN24ior20$',
+  database: 'bakery'
+});
 
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to MySQL database!");
+});
+
+// Don't import the `con` object from a separate file; use the `connection` object instead
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
-
 app.listen(3000);
 
-
-
-//Reads out the information 
-/*con.getConnection(function(err) {
-    if (err) throw err;
-    con.query("SELECT * FROM product", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-    });
-  });*/
-  
-
 app.post("/", function(request, response){
-    var reviewRate = req.body.reviewRate;
-    var review = req.body.review;
+    var reviewRate = request.body.reviewRate;
+    var review = request.body.review;
 
     console.log(reviewRate, review);
 
-    var sql = 'INSERT into review (reviewRating, reviewDesc) values ?';
+    var sql = 'INSERT INTO review (reviewRating, reviewDesc) VALUES (?, ?)';
+    var values = [reviewRate, review];
 
-    var values = [[reviewRate, review]];
-    
-    con.query(sql, [values], function(err, result){
+    // Use the `connection` object to execute the query, not the `con` object
+    connection.query(sql, values, function(err, result){
         if(err) throw err;
 
         console.log("Data uploaded");
         response.redirect('/');
     });
+});
+
+app.post("/orders", function(request, response){
+  var name = request.body.name;
+  var email = request.body.email;
+  var phone = request.body.phone;
+  var address = request.body.address;
+  var order = request.body.order;
+
+  console.log(name, email, phone, address, order);
+
+  var sql = 'INSERT INTO orders (name, email, phone, address, order_details) VALUES (?, ?, ?, ?, ?)';
+  var values = [name, email, phone, address, order];
+
+  connection.query(sql, values, function(err, result){
+      if(err) throw err;
+
+      console.log("Order uploaded");
+      response.redirect('/');
+  });
 });
