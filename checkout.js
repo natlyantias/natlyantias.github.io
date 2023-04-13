88c144c101da879e329712c0cc0e8a9054f511f0
natlyantias <98179490+natlyantias@users.noreply.github.com>
2023-04-13 17:14:17 -0400

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,45 +1,46 @@
 const cart = [];
 
 function redirectToCheckout() {
-window.location.href = "checkoutindex.html";
+  window.location.href = "checkoutindex.html";
 }
 
 document.getElementById("checkout-button").addEventListener("click", redirectToCheckout);
 
 const menuItems = document.querySelectorAll('.menu-item');
+
 menuItems.forEach(item => {
-const addToCartButton = item.querySelector('.add-to-cart');
-addToCartButton.addEventListener('click', () => {
-const name = item.dataset.name;
-const price = item.dataset.price;
-addToCart(name, price);
-});
+  const addToCartButton = item.querySelector('.add-to-cart');
+  addToCartButton.addEventListener('click', () => {
+    const name = item.dataset.name;
+    const price = item.dataset.price;
+    addToCart(name, price);
+  });
 });
 
 const orderSummaryElement = document.querySelector('#order-summary');
 const totalElement = document.querySelector('#total');
 
 function addToCart(name, price) {
-const item = { name, price };
-cart.push(item);
-updateOrderSummary();
+  const item = { name, price };
+  cart.push(item);
+  updateOrderSummary();
 }
 
 function updateOrderSummary() {
-let total = 0;
-let html = '';
-cart.forEach(item => {
-html += <li>${item.name} - ${item.price}</li>;
-total += parseFloat(item.price);
-});
-html += <li>Total: $${total.toFixed(2)}</li>;
+  let total = 0;
+  let html = '';
+  cart.forEach(item => {
+    html += `<li>${item.name} - ${item.price}</li>`;
+    total += parseFloat(item.price);
+  });
+  html += `<li>Total: $${total.toFixed(2)}</li>`;
 
-orderSummaryElement.innerHTML = html;
-totalElement.textContent = Total: $${total.toFixed(2)};
+  orderSummaryElement.innerHTML = html;
+  totalElement.textContent = `Total: $${total.toFixed(2)}`;
 
-// set the total dynamically
-const totalAmountElement = document.getElementById('total-amount');
-totalAmountElement.textContent = total.toFixed(2);
+  // set the total dynamically
+  const totalAmountElement = document.getElementById('total-amount');
+  totalAmountElement.textContent = total.toFixed(2);
 }
 
 // checkout.js
@@ -52,15 +53,15 @@ const checkoutSummaryElement = document.querySelector('#checkout-summary');
 
 let checkoutHtml = '';
 selectedItems.forEach(item => {
-checkoutHtml += <li>${item.name} - ${item.price} - Quantity: ${item.quantity}</li>;
+  checkoutHtml += `<li>${item.name} - ${item.price} - Quantity: ${item.quantity}</li>`;
 });
 
 checkoutSummaryElement.innerHTML = checkoutHtml;
 
 let totalPrice = 0;
 selectedItems.forEach(item => {
-totalPrice += item.price * item.quantity;
+  totalPrice += item.price * item.quantity;
 });
 
 const checkoutTotalElement = document.querySelector('#checkout-total');
-checkoutTotalElement.textContent = Total: $${totalPrice.toFixed(2)};
+checkoutTotalElement.textContent = `Total: $${totalPrice.toFixed(2)}`;
