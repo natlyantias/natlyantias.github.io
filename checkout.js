5c6657085400a21ae6c9d61909c8dd6fca213173
natlyantias <98179490+natlyantias@users.noreply.github.com>
2023-04-13 17:20:34 -0400

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,52 +1,3 @@
-const cart = [];
-
-function redirectToCheckout() {
-  const checkoutUrl = new URL('checkoutindex.html', window.location.href);
-  checkoutUrl.searchParams.set('items', JSON.stringify(cart));
-  checkoutUrl.searchParams.set('total', getTotalPrice());
-  window.location.href = checkoutUrl.toString();
-}
-
-document.getElementById("checkout-button").addEventListener("click", redirectToCheckout);
-
-const menuItems = document.querySelectorAll('.menu-item');
-
-menuItems.forEach(item => {
-  const addToCartButton = item.querySelector('.add-to-cart');
-  addToCartButton.addEventListener('click', () => {
-    const name = item.dataset.name;
-    const price = item.dataset.price;
-    addToCart(name, price);
-  });
-});
-
-const orderSummaryElement = document.querySelector('#order-summary');
-const totalElement = document.querySelector('#total');
-
-function addToCart(name, price) {
-  const item = { name, price };
-  cart.push(item);
-  updateOrderSummary();
-}
-
-function updateOrderSummary() {
-  let total = 0;
-  let html = '';
-  cart.forEach(item => {
-    html += `<li>${item.name} - ${item.price}</li>`;
-    total += parseFloat(item.price);
-  });
-  html += `<li>Total: $${total.toFixed(2)}</li>`;
-
-  orderSummaryElement.innerHTML = html;
-  totalElement.textContent = `Total: $${total.toFixed(2)}`;
-
-  // set the total dynamically
-  const totalAmountElement = document.getElementById('total-amount');
-  totalAmountElement.textContent = total.toFixed(2);
-}
-
-// checkout.js
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
@@ -61,10 +12,6 @@ selectedItems.forEach(item => {
 
 checkoutSummaryElement.innerHTML = checkoutHtml;
 
-let totalPrice = 0;
-selectedItems.forEach(item => {
-  totalPrice += item.price * item.quantity;
-});
-
+const totalPrice = urlParams.get('total');
 const checkoutTotalElement = document.querySelector('#checkout-total');
-checkoutTotalElement.textContent = `Total: $${totalPrice.toFixed(2)}`;
+checkoutTotalElement.textContent = `Total: $${totalPrice}`;
