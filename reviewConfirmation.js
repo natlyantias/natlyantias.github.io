b7d3791acdd7d1f8c2ece5f3462b3d696b6dc4e7
natlyantias <98179490+natlyantias@users.noreply.github.com>
2023-04-14 18:29:25 -0400

    Create reviewConfirmation.js

---
+++ reviewConfirmation.js
@@ -0,0 +1,6 @@
+
+function redirectToReviewConfirmation() {
+  window.location.href = "reviewConfirmationindex.html";
+}
+
+document.getElementById("reviewConfirmation-button").addEventListener("click", redirectToReviewConfirmation);
